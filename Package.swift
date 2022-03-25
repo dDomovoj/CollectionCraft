170fe08b15229c545dcedd5caa3fbd231284b117
dDomovoj <dmitryduleba@gmail.com>
2022-03-25 13:49:38 +0100

    [upd] package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
   platforms: [SupportedPlatform.iOS(.v11)],
   products: [.library(name: "CollectionCraft", targets: ["CollectionCraft"])],
   dependencies: [.package(url: "https://github.com/ra1028/DifferenceKit", .upToNextMajor(from: .init(1, 2, 0)))],
-  targets: [.target(name: "CollectionCraft", dependencies: [], path: "Sources/CollectionCraft")],
+  targets: [.target(name: "CollectionCraft", dependencies: ["DifferenceKit"], path: "Sources/CollectionCraft")],
   swiftLanguageVersions: [.v5]
 )
